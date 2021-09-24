5b81ac26b3df62040acbcbb74ffe2fb61eec916e
divinapham <91284022+divinapham@users.noreply.github.com>
2021-09-24 10:48:43 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "shark":return img`
+.............88fff..............
+............8dd99f..............
+...........8dd99f...............
+..........f88998f............888
+....ffffff888888ff.........88998
+..ff99999999999998fff.....8d998.
+ff9999999998989999888ff..8dd99f.
+f9899999ff9989899988888fffd99f..
+f9991111ff1989899988888889998f..
+.f911111111999999888888888988f..
+..f888338811999988888888fff998f.
+...f81318111999888889d98...f99f.
+....f3381118999fddddd88.....f99f
+.....ff1111f9d99fdd88........fff
+.......88888f9d99f8.............
+.............fffff..............
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,28 @@
+let projectile2: Sprite = null
+let shark = sprites.create(assets.image`shark`, SpriteKind.Player)
+controller.moveSprite(shark)
+scene.setBackgroundColor(6)
+shark.setStayInScreen(true)
+effects.bubbles.startScreenEffect()
+info.setScore(0)
+info.startCountdown(10)
+game.onUpdateInterval(500, function () {
+    projectile2 = sprites.createProjectileFromSide(img`
+        . . . . . . . 6 . . . . . . . . 
+        . . . . . . 8 6 6 . . . 6 8 . . 
+        . . . e e e 8 8 6 6 . 6 7 8 . . 
+        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+        e e 2 e 2 2 4 2 2 e e e c . . . 
+        e e e e 2 e 2 2 e e e c . . . . 
+        e e e 2 e e c e c c c . . . . . 
+        . c c c c c c c . . . . . . . . 
+        `, 50, 50)
+})
